ba4db070e43e8b179e29c177c8610fadd6effb82
Samir <sam1r.aslanoff.as@gmail.com>
2025-01-19 20:25:33 +0200

    finished routing (5.3.0)

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,21 +1,27 @@
+import { NavLink, Link } from 'react-router-dom';
+
 import './appHeader.scss';
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="#">
+        <Link to="/">
           <span>Marvel</span> information portal
-        </a>
+        </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="#">Characters</a>
+            <NavLink exact to="/" activeClassName="selected">
+              Characters
+            </NavLink>
           </li>
           /
           <li>
-            <a href="#">Comics</a>
+            <NavLink exact to="/comics" activeClassName="selected">
+              Comics
+            </NavLink>
           </li>
         </ul>
       </nav>
