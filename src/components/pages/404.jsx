a92c1be81409a9fcea3a85b9fcfd57cbd0eb5e4a
Samir <sam1r.aslanoff.as@gmail.com>
2025-01-20 16:29:57 +0200

    changed 404 page

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h2>404</h2>
       <p>This is not the web page you are looking for.</p>
+      <Link
+        to="/"
+        style={{
+          display: 'inline-block',
+          backgroundColor: '#9F0013',
+          color: '#fff',
+          padding: '8px',
+          margin: '10px',
+          borderRadius: '4px',
+        }}
+      >
+        Return to main page
+      </Link>
     </div>
   );
 };
