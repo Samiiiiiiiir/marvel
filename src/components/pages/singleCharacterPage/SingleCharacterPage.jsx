7a7cb024ec6c62b83b0eb899eee87577e6911f73
Samir <sam1r.aslanoff.as@gmail.com>
2025-01-29 19:13:25 +0200

    finished doing singleChar page

--- src/components/pages/singleCharacterPage/SingleCharacterPage.jsx
+++ src/components/pages/singleCharacterPage/SingleCharacterPage.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from 'react';
+
+import { useParams, Link } from 'react-router-dom';
+import useMarvelService from '../../../services/MarvelService';
+import Spinner from '../../spinner/Spinner';
+import ErrorMessage from '../../errorMessage/ErrorMessage';
+
 import './singleCharacterPage.scss';
 
 const SingleCharacterPage = () => {
-  return <div>Single character</div>;
+  const { characterId } = useParams();
+  const { status, getOneCharacter } = useMarvelService();
+  const [char, setChar] = useState();
+
+  useEffect(() => {
+    getOneCharacter(characterId).then(onLoaded);
+  }, []);
+
+  const onLoaded = (res) => {
+    setChar(res);
+  };
+
+  return (
+    <>
+      {status == 'loading' ? <Spinner /> : null}
+      {status == 'error' ? <ErrorMessage /> : null}
+      {status == 'loaded' ? <View char={char} /> : null}
+    </>
+  );
+};
+
+const View = ({ char }) => {
+  const { name, thumbnail, description } = char;
+  return (
+    <div className="single-char">
+      <img
+        src={thumbnail}
+        alt=""
+        className="single-char__img"
+        width={293}
+        height={293}
+      />
+      <div className="single-char__desc">
+        <h2 className="single-char__name">{name}</h2>
+        <div className="single-char__text">
+          <p>{description}</p>
+        </div>
+        <Link to="/" className="single-char__back">
+          Go back
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SingleCharacterPage;
